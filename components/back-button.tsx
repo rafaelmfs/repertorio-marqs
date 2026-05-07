896b4d7ce2e89c098724881cc7358ba891243d03
Rafael Marques <rafael.marques-98@hotmail.com>
2026-05-07 18:03:59 -0300

    feat: refactor BackButton to use handleBack function for navigation logic

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -8,8 +8,20 @@ import { Button } from "./ui/button";
 export function BackButton(props: Omit<ComponentProps<typeof Button>, 'onClick'>) {
   const navigate = useRouter();
 
+  function handleBack() {
+    const referrer = document.referrer;
+    const isInternalNavigation = referrer && new URL(referrer).origin === window.location.origin;
+
+    if (isInternalNavigation) {
+      navigate.back();
+      return
+    }
+
+    navigate.push('/');
+  }
+
   return (
-    <Button onClick={() => navigate.back()} variant="outline" size="sm" {...props}>
+    <Button onClick={handleBack} variant="outline" size="sm" {...props}>
       Voltar
     </Button>
   )
